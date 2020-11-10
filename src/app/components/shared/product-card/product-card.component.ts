4dc4d66e2eefa8c6e603294ff6d3600b444d183b
ShadeB <shadebanda2@gmail.com>
2020-11-10 14:09:05 +0200

    Add input decorator to recieve product data to be displayed in the product card component

--- src/app/components/shared/product-card/product-card.component.ts
+++ src/app/components/shared/product-card/product-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../../../Interfaces/product';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-  stars = [1,2,3,4,5];
+  @Input() product: any;
+  stars = [1, 2, 3, 4, 5];
   rating = 1;
   hoverState = 0;
   constructor() { }
