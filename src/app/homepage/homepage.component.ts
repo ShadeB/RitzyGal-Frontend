adf64dc80c3a7bb7acb1195955e51ee133fa3e0f
ShadeB <shadebanda2@gmail.com>
2020-11-10 13:56:11 +0200

    Get all products on homepage load

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Apollo } from 'apollo-angular';
+import { AllProductsQuery } from '../graphql/queries/allProductsQuery';
+import { Product } from '../Interfaces/product';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  products: Product[];
+  loading: boolean = true;
+  errors: any;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apollo
+      .query<any>({
+        query: AllProductsQuery
+      })
+      .subscribe(
+        ({ data, error, loading }) => {
+          this.products = data.listAllProducts;
+          this.loading = loading;
+          this.errors = error;
+        })
   }
 
 }
