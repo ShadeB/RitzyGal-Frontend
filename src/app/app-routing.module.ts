ced2f0edbb477dc274bcb3dfef6c9828b5748718
ShadeB <shadebanda2@gmail.com>
2020-11-24 16:10:57 +0200

    create add to cart route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from './homepage/homepage.component';
+import { AddToCartComponent } from './components/shared/add-to-cart/add-to-cart.component';
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'home', component: HomepageComponent},
+  {path: 'add-to-cart', component: AddToCartComponent},
 ];
 
 @NgModule({
