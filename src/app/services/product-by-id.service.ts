4a8b4eaa3b9198bcf8a43c392db8a60157748322
ShadeB <shadebanda2@gmail.com>
2021-01-06 20:36:58 +0200

    Return an Observable of Product in the product by id service

--- src/app/services/product-by-id.service.ts
+++ src/app/services/product-by-id.service.ts
@@ -1,9 +1,10 @@
 import {Observable} from 'rxjs';
 import { Injectable } from '@angular/core';
 import {Apollo} from 'apollo-angular';
+import { map } from 'rxjs/operators';
 
 import { productByIdQuery } from 'src/app/graphql/queries/productById';
-
+import { Product } from 'src/app/Interfaces/product';
 
 
 @Injectable({
@@ -14,10 +15,13 @@ export class ProductByIdService{
   constructor(private apollo: Apollo) {
   }
 
-  getProduct(id: string): Observable<any> {
-    return this.apollo.query<any>({
+  getProduct(id: string): Observable<Product> {
+    return this.apollo.query<productByIdQuery>({
       query: productByIdQuery,
       variables: {id}
-    })
+    }).pipe(map(result => {
+      let product = { ...result.data.findProductById }
+      return product;
+    }))
   }
 }
