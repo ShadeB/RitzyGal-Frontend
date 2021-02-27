aacc2b6d89a3664050524c186bf657927a975570
ShadeB <shadebanda2@gmail.com>
2021-02-27 21:02:19 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'secondary-color': '#ff00e5',
+      }
+    },
+  },
+  variants: {
+    scale: ['responsive', 'hover', 'focus', 'group-hover'],
+    extend: {},
+  },
+  plugins: [],
+}
