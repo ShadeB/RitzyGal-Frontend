a49ca96720b496ae0af8f1664e0dd6eb0d740200
ShadeB <shadebanda2@gmail.com>
2021-03-29 12:37:25 +0200

    Remove unused import of Input module

--- src/app/components/cart-side-widget/cart-side-widget.component.ts
+++ src/app/components/cart-side-widget/cart-side-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {CartService} from 'src/app/services/cart.service';
 import {CartWidgetService} from 'src/app/services/cart-widget.service';
